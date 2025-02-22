de8c0f7c15241133945e016d1db4a4c2303a44c4
RishabhS21 <rishabh80gupta@gmail.com>
2025-02-22 16:46:30 +0530

    feat: added support for nr_rlc_dl_stats pkt v3.0

--- dm_collector_c/nr_rlc_dl_stats.h
+++ dm_collector_c/nr_rlc_dl_stats.h
@@ -7,77 +7,122 @@
 #include "log_packet_helper.h"
 
 const Fmt NrRlcDlStats_Fmt [] = {
-    {UINT, "Version", 4},
+    {UINT, "Minor Version", 2},
+    {UINT, "Major Version", 2},
+
 };
 
 const Fmt NrRlcDlStats_v4 [] = {
+    {UINT, "Num RB",                   1},
+    {UINT, "Reason",                   1},
+    {UINT, "Num FC Down Commands",     4},
+    {UINT, "Num FC Up Commands",       4},
+    {UINT, "Num Shutdown Issued",      4},
+    {UINT, "Num RLF Issued",           4},
+    {UINT, "Num Level1 Down Commands", 4},
+    {UINT, "Num Level1 Up Commands",   4},
+    {UINT, "Num Scell TA Start",       4},
+    {UINT, "Num Scell TA Stop",        4},
+    {UINT, "Num Scell FC Start",       4},
+    {UINT, "Num Scell FC Stop",        4},
+    {UINT, "Num RF Available",         4},
+    {UINT, "Num FC RX Win",            4},
+    {UINT, "Last RLF",                 4},  
+};
+
+const Fmt NrRlcDlStats_v3 [] = {
     {UINT, "Num RB",               1},
     {UINT, "Reason",               1},
     {UINT, "Num FC Down Commands", 4},
     {UINT, "Num FC Up Commands",   4},
-    {UINT, "Num Shutdown Issued",  4},
     {UINT, "Num RLF Issued",       4},
-    {UINT, "Num Level1 Down Commands", 4},
-    {UINT, "Num Level1 Up Commands",   4},
     {UINT, "Num Scell TA Start",   4},
     {UINT, "Num Scell TA Stop",    4},
     {UINT, "Num Scell FC Start",   4},
     {UINT, "Num Scell FC Stop",    4},
     {UINT, "Num RF Available",     4},
     {UINT, "Num FC RX Win",        4},
-    {UINT, "Last RLF",             4},  
+    {UINT, "Last RLF",             4},
 };
 
-//Num FC Level1 Inc Segs Dropped List[4]
-const Fmt NrRlcDlStats_NumFCLevel1IncSegDrop_v4 [] = {
+const Fmt NrRlcDlStats_NumFCLevel1IncSegDrop [] = {
     {UINT, "Num FC Level1 Inc Segs Dropped[0]", 4},
     {UINT, "Num FC Level1 Inc Segs Dropped[1]", 4},
     {UINT, "Num FC Level1 Inc Segs Dropped[2]", 4},
     {UINT, "Num FC Level1 Inc Segs Dropped[3]", 4},
 };
 
-const Fmt NrRlcDlStats_Others_v4 [] = {
-    {UINT, "Num FC Level1 History Index", 4},
+const Fmt NrRlcDlStats_Others [] = {
+    {UINT, "Num FC Level1 History Index",      4},
     {UINT, "Num FC Shutdown Inc Segs Dropped", 4},
-    {SKIP, NULL, 2},
+    {SKIP, NULL,                               2},
 };
 
 //RB Stats Based on Num RB 
 const Fmt NrRlcDlStats_RBStats_v4 [] = {
-    {SKIP, NULL,               1},
-    {UINT, "RB Mode",          1},
-    {SKIP, NULL,               4},
-    {UINT, "Num Data PDU",     4},
-    {UINT, "Num Stats Rxed",   4},
-    {UINT, "Num Invalid PDUS", 4},
-    {UINT, "Num ReTX PDU",     4},
-    {UINT, "Num Duplicate PDU",4},
-    {UINT, "Num Dropped PDU",  4},
-    {UINT, "Num Segment Join", 4},
-    {UINT, "Num Segment Collapse", 4},
-    {UINT, "Num Status Trigger",   4},
-    {UINT, "Num Status Update", 4},
-    {UINT, "Num Status OTA",    4},
-    {UINT, "Num Status PDU Drop",  4},
-    {UINT, "Num Reassembly Expired",4},
-    {UINT, "Num Reassembly Start", 4},
-    {UINT, "Num Missed UM PDU",    4},
-    {SKIP, NULL,                 4},
+    {SKIP, NULL,                             1},
+    {UINT, "RB Mode",                        1},
+    {SKIP, NULL,                             4},
+    {UINT, "Num Data PDU",                   4},
+    {UINT, "Num Stats Rxed",                 4},
+    {UINT, "Num Invalid PDUS",               4},
+    {UINT, "Num ReTX PDU",                   4},
+    {UINT, "Num Duplicate PDU",              4},
+    {UINT, "Num Dropped PDU",                4},
+    {UINT, "Num Segment Join",               4},
+    {UINT, "Num Segment Collapse",           4},
+    {UINT, "Num Status Trigger",             4},
+    {UINT, "Num Status Update",              4},
+    {UINT, "Num Status OTA",                 4},
+    {UINT, "Num Status PDU Drop",            4},
+    {UINT, "Num Reassembly Expired",         4},
+    {UINT, "Num Reassembly Start",           4},
+    {UINT, "Num Missed UM PDU",              4},
+    {SKIP, NULL,                             4},
     {UINT, "Num Status Trigger Ack SN Only", 4},
     {UINT, "Num Status Trigger Nack List",   4},
     {UINT, "Num Status Update Ack SN Only",  4},
     {UINT, "Num Status Update Nack List",    4},
-    {UINT, "Num Status Ackd",   4},
-    {UINT, "Num Status Nack Lists", 4},
-    {UINT, "RLC Data Bytes",    4},
-    {SKIP, NULL, 2},
+    {UINT, "Num Status Ackd",                4},
+    {UINT, "Num Status Nack Lists",          4},
+    {UINT, "RLC Data Bytes",                 4},
+    {SKIP, NULL,                             2},
 };
 
+const Fmt NrRlcDlStats_RBStats_v3 [] = {
+    {UINT, "RB Config Index",                1},
+    {UINT, "RB Mode",                        1},
+    {UINT, "Num RLC Meta",                   4},
+    {UINT, "Num Data PDU",                   4},
+    {UINT, "Num Stats Rxed",                 4},
+    {UINT, "Num Invalid PDUS",               4},
+    {UINT, "Num ReTX PDU",                   4},
+    {UINT, "Num Duplicate PDU",              4},
+    {UINT, "Num Dropped PDU",                4},
+    {UINT, "Num Segment Join",               4},
+    {UINT, "Num Segment Collapse",           4},
+    {UINT, "Num Status Trigger",             4},
+    {UINT, "Num Status Update",              4},
+    {UINT, "Num Status OTA",                 4},
+    {UINT, "Num Status PDU Drop",            4},
+    {UINT, "Num Reassembly Expired",         4},
+    {UINT, "Num Reassembly Start",           4},
+    {UINT, "Num Missed UM PDU",              4},
+    {UINT, "Num Meta Overlap",               4},
+    {UINT, "Num Status Trigger Ack SN Only", 4},
+    {UINT, "Num Status Trigger Nack List",   4},
+    {UINT, "Num Status Update Ack SN Only",  4},
+    {UINT, "Num Status Update Nack List",    4},
+    {UINT, "Num Status Ackd",                4},
+    {UINT, "Num Status Nack Lists",          4},
+    {UINT, "RLC Data Bytes",                 4},
+    {SKIP, NULL,                             2},
+};
 static int _decode_nr_rlc_dl_status_payload(const char *b,
 		int offset, size_t length, PyObject *result){	
     int start = offset;
-    int pkt_ver = _search_result_int(result, "Version");
-
+    int pkt_ver = _search_result_int(result, "Major Version");
+    int minor_ver = _search_result_int(result, "Minor Version");
     PyObject *old_object;
     PyObject *pyfloat;
 
@@ -91,8 +136,8 @@ static int _decode_nr_rlc_dl_status_payload(const char *b,
 
        //Num FC Level1 Inc Segs Dropped List[4]
        PyObject* result_NumFCLevel1IncSegDrop_item = PyList_New(0);
-       offset += _decode_by_fmt(NrRlcDlStats_NumFCLevel1IncSegDrop_v4,
-           ARRAY_SIZE(NrRlcDlStats_NumFCLevel1IncSegDrop_v4, Fmt),
+       offset += _decode_by_fmt(NrRlcDlStats_NumFCLevel1IncSegDrop,
+           ARRAY_SIZE(NrRlcDlStats_NumFCLevel1IncSegDrop, Fmt),
            b, offset, length, result_NumFCLevel1IncSegDrop_item);
        PyObject* t0 = Py_BuildValue("(sOs)", "Num FC Level1 Inc Segs Dropped",
            result_NumFCLevel1IncSegDrop_item, "dict");
@@ -100,8 +145,8 @@ static int _decode_nr_rlc_dl_status_payload(const char *b,
        Py_DECREF(t0);
        Py_DECREF(result_NumFCLevel1IncSegDrop_item);
 
-       offset += _decode_by_fmt(NrRlcDlStats_Others_v4,
-           ARRAY_SIZE(NrRlcDlStats_Others_v4, Fmt),
+       offset += _decode_by_fmt(NrRlcDlStats_Others,
+           ARRAY_SIZE(NrRlcDlStats_Others, Fmt),
            b, offset, length, result);
        PyObject* result_NrRlcDlStats_RBStats = PyList_New(0);
        for (int i = 0; i < num_rb; i++) {
@@ -110,7 +155,49 @@ static int _decode_nr_rlc_dl_status_payload(const char *b,
                ARRAY_SIZE(NrRlcDlStats_RBStats_v4, Fmt),
                b, offset, length, result_NrRlcDlStats_RBStats_item);
            PyObject* t2 = Py_BuildValue("(sOs)", "Ignored",
-               result_NrRlcDlStats_RBStats_item, "dict");
+			   result_NrRlcDlStats_RBStats_item, "dict");
+	   PyList_Append(result_NrRlcDlStats_RBStats, t2);
+           Py_DECREF(t2);
+           Py_DECREF(result_NrRlcDlStats_RBStats_item);
+       }
+
+       PyObject* t3 = Py_BuildValue("(sOs)", "RB Stats",
+           result_NrRlcDlStats_RBStats, "list");
+       PyList_Append(result, t3);
+       Py_DECREF(t3);
+       Py_DECREF(result_NrRlcDlStats_RBStats);
+       break;
+
+    }
+    case 3:
+    {
+       offset += _decode_by_fmt(NrRlcDlStats_v3,
+           ARRAY_SIZE(NrRlcDlStats_v3, Fmt),
+           b, offset, length, result);
+       int num_rb = _search_result_int(result, "Num RB");
+
+       //Num FC Level1 Inc Segs Dropped List[4]
+       PyObject* result_NumFCLevel1IncSegDrop_item = PyList_New(0);
+       offset += _decode_by_fmt(NrRlcDlStats_NumFCLevel1IncSegDrop,
+           ARRAY_SIZE(NrRlcDlStats_NumFCLevel1IncSegDrop, Fmt),
+           b, offset, length, result_NumFCLevel1IncSegDrop_item);
+       PyObject* t0 = Py_BuildValue("(sOs)", "Num FC Level1 Inc Segs Dropped",
+           result_NumFCLevel1IncSegDrop_item, "dict");
+       PyList_Append(result, t0);
+       Py_DECREF(t0);
+       Py_DECREF(result_NumFCLevel1IncSegDrop_item);
+
+       offset += _decode_by_fmt(NrRlcDlStats_Others,
+           ARRAY_SIZE(NrRlcDlStats_Others, Fmt),
+           b, offset, length, result);
+       PyObject* result_NrRlcDlStats_RBStats = PyList_New(0);
+       for (int i = 0; i < num_rb; i++) {
+           PyObject* result_NrRlcDlStats_RBStats_item = PyList_New(0);
+           offset += _decode_by_fmt(NrRlcDlStats_RBStats_v3,
+               ARRAY_SIZE(NrRlcDlStats_RBStats_v3, Fmt),
+               b, offset, length, result_NrRlcDlStats_RBStats_item);
+           PyObject* t2 = Py_BuildValue("(sOs)", "Ignored",
+                           result_NrRlcDlStats_RBStats_item, "dict");
            PyList_Append(result_NrRlcDlStats_RBStats, t2);
            Py_DECREF(t2);
            Py_DECREF(result_NrRlcDlStats_RBStats_item);
